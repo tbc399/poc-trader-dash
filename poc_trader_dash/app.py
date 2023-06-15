be18ef09d256ed85ccbd3b77742bf50204e0b33d
Travis Cammack <traviscammack@protonmail.com>
2023-06-15 07:13:57 -0500

    got the dash server to run and display basic performance graph

--- poc_trader_dash/app.py
+++ poc_trader_dash/app.py
@@ -1,8 +1,9 @@
 import asyncio
+from dash import Dash, html, dcc, callback, Output, Input
 from collections import defaultdict
 from os import environ
 
-import asyncpg
+import psycopg
 import pandas as pd
 import yfinance
 from plotly import express as px
@@ -18,22 +19,37 @@ def percent_change(start, finish):
     return ((finish - start) / start) * 100
 
 
-async def run():
-    connection = await asyncpg.connect(dsn=db_url)
-    results = await connection.fetch(
-        "select account_number, date, close from account_close_snapshots order by date asc",
-    )
-    await connection.close()
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        dcc.Location(id='url'),
+        html.H1(children="Returns", style={"textAlign": "center"}),
+        # dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
+        dcc.Graph(id="graph-content"),
+    ]
+)
+
+
+@callback(Output("graph-content", "figure"), Input("url", 'pathname'))
+def show_graph(pathname):
+    connection = psycopg.connect(db_url)
+    with connection.cursor() as cur:
+        results = cur.execute(
+            "select account_number, date, close from account_close_snapshots order by date asc",
+        ).fetchall()
+    connection.close()
 
     account_returns = defaultdict(list)
     for record in results:
-        account_returns[record.get("account_number")].append(
-            (record.get("date"), record.get("close"))
+        account_returns[record[0]].append(
+            (record[1], record[2])
         )
     dfs = []
     for account, closing_values in account_returns.items():
         returns = [
-            (date, percent_change(closing_values[0][1], value)) for date, value in closing_values
+            (date, percent_change(closing_values[0][1], value))
+            for date, value in closing_values
         ]
         dfs.append(pd.DataFrame(returns, columns=["date", account]).set_index("date"))
 
@@ -42,7 +58,8 @@ async def run():
     )
 
     spx_returns = [
-        (date.date(), percent_change(spx.values[0][0], value[0])) for date, value in spx.iterrows()
+        (date.date(), percent_change(spx.values[0][0], value[0]))
+        for date, value in spx.iterrows()
     ]
     dfs.append(pd.DataFrame(spx_returns, columns=["date", "SPX"]).set_index("date"))
     fig = px.line(pd.concat(dfs, axis=1), title="Returns")
@@ -51,32 +68,8 @@ async def run():
             dict(bounds=["sat", "mon"]),  # hide weekends
         ]
     )
-    fig.show()
+    return fig
 
 
-
-
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-
-app = Dash(__name__)
-
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-])
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
